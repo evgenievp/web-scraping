37b62b9e8d681473bee5a3f8329fb0a6cf4aed98
petar <evgenievp@yahoo.com>
2024-10-20 10:33:43 +0300

    I enjoy reading my zodiac sign info, so I scrape daily horoscope information from the zodiac.dir website.

--- zodiac.py
+++ zodiac.py
@@ -1,19 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://zodiac.dir.bg/sign/lav/dneven-horoskop'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html.parser')
-container = soup.find_all('div', 'article-body horoscope')
-res = ''
-stars = ''
-for el in container:
-    ul = el.find('ul')
-    stars += ul.text
-    e = el.find('p')
-    res += e.text
 
-res = res.replace(".", '.\n')
-stars = stars.replace("\n", "")
-print(res)
-print(stars)
+def zodiac_read():
+    url = 'https://zodiac.dir.bg/sign/lav/dneven-horoskop'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    container = soup.find_all('div', 'article-body horoscope')
+    res = ''
+    stars = ''
+    for el in container:
+        ul = el.find('ul')
+        stars += ul.text
+        e = el.find('p')
+        res += e.text
+
+    res = res.replace(".", '.\n')
+    stars = stars.replace("\n", "")
+    print(res)
+    print(stars)
+
+
+if __name__ == "__main__":
+    zodiac_read()
+    input("Press some key to exit.")
