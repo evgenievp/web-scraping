2bc6e224b4d31e6347d8f61233d913228da2e9ec
petar evtimov <evgenievp@yahoo.com>
2025-02-15 07:57:38 +0200

    Scrape weather from sinoptik.bg - current temperature and how it feels with 14 days prognosis.

--- weather.py
+++ weather.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 place = 'София'
-
+url = 'https://www.sinoptik.bg/sofia-bulgaria-100727011'
 WINDOW_SIZE = '1920, 1080'
 
 chrome_options = webdriver.ChromeOptions()
@@ -15,7 +15,6 @@ chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
 chrome = webdriver.Chrome(chrome_options)
 
-url = 'https://www.sinoptik.bg/sofia-bulgaria-100727011'
 chrome.get(url=url)
 
 ok_btn = chrome.find_element(By.XPATH, '/html/body/div[8]/div[2]/div[2]/div[2]/div[2]/button[1]').click()
@@ -24,8 +23,29 @@ feel_like = chrome.find_element(By.XPATH, '//*[@id="wfCurrent"]/div/span[2]')
 
 now_temp = chrome.find_element(By.XPATH, '//*[@id="wfCurrent"]/div/span[1]/span')
 
-print("Температура в София:", now_temp.text)
-print(feel_like.text)
+temperature = ''
+temperature += f"Температура в София:, {now_temp.text}\n"
+temperature += f"Чувства се като: {feel_like.text}\n"
+chrome.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[1]/div[4]/ul/li[5]/a').click()
+
+time.sleep(1)
+
+days_14 = chrome.find_element(By.XPATH, '//*[@id="wf10day"]/div[2]/div/div/div')
+
+result = []
+for el in days_14.text:
+    result.append(el)
+
+text = ''
+for el in result:
+    if el in "\n ":
+        text += " "
+    if el == "s":
+        text += "s\n"
+    else:
+        text += el
+
+print(text)
 
 chrome.quit()
 
